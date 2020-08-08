3f6f8c18d4f493220ca7c3edb95ff0fed3d9d8d1
Yu <yu.smc.roundabout@gmail.com>
2020-08-08 18:17:28 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// --vhというカスタムプロパティを作成
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+window.addEventListener("resize", () => {
+  vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
