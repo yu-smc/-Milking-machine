a4bec5f057292c7e88823941de62639a12fabd58
Yu <yu.smc.roundabout@gmail.com>
2020-08-08 19:28:35 +0900

    display when pc

--- main.js
+++ main.js
@@ -6,3 +6,17 @@ window.addEventListener("resize", () => {
   vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
 });
+
+if (
+  navigator.userAgent.indexOf("iPhone") > 0 ||
+  (navigator.userAgent.indexOf("Android") > 0 &&
+    navigator.userAgent.indexOf("Mobile") > 0)
+) {
+} else if (
+  navigator.userAgent.indexOf("iPad") > 0 ||
+  navigator.userAgent.indexOf("Android") > 0
+) {
+} else {
+  document.getElementById("ar-description-text").style.display = "none";
+  document.getElementById("pc-alert-text").style.display = "block";
+}
