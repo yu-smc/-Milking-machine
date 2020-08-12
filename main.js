fe3ef47bd4745a45850ecc59b81504e41b804145
Yu <yu.smc.roundabout@gmail.com>
2020-08-12 12:40:48 +0900

    fix ua logic

--- main.js
+++ main.js
@@ -7,25 +7,23 @@ window.addEventListener("resize", () => {
   document.documentElement.style.setProperty("--vh", `${vh}px`);
 });
 
+const ua = window.navigator.userAgent.toLowerCase();
+
 if (
-  navigator.userAgent.indexOf("iPhone") > 0 ||
-  (navigator.userAgent.indexOf("Android") > 0 &&
-    navigator.userAgent.indexOf("Mobile") > 0)
+  ua.indexOf("iphone") > 0 ||
+  (ua.indexOf("android") > 0 && ua.indexOf("mobile") > 0)
 ) {
 } else if (
-  navigator.userAgent.indexOf("iPad") > 0 ||
-  (navigator.userAgent.indexOf("macintosh") > -1 && "ontouchend" in document) ||
-  navigator.userAgent.indexOf("Android") > 0
+  ua.indexOf("ipad") > 0 ||
+  (ua.indexOf("macintosh") > -1 && "ontouchend" in document) ||
+  ua.indexOf("android") > 0
 ) {
 } else {
   document.getElementById("ar-description-text").style.display = "none";
   document.getElementById("pc-alert-text").style.display = "block";
 }
 
-if (
-  navigator.userAgent.indexOf("iPhone") > 0 ||
-  navigator.userAgent.indexOf("iPad") > 0
-) {
+if (ua.indexOf("iphone") > 0 || ua.indexOf("ipad") > 0) {
   document.getElementById("model-viewer-ios").style.display = "block";
   document.getElementById("model-viewer-default").style.display = "none";
 }
