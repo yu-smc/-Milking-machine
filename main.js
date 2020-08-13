e16fc4693f0dbc0730ccfab6daae00d592bcc228
Yu <yu.smc.roundabout@gmail.com>
2020-08-13 12:40:48 +0900

    ar messaeg

--- main.js
+++ main.js
@@ -2,6 +2,12 @@
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty("--vh", `${vh}px`);
 
+const viewer = document.getElementById("model-viewer");
+
+const arDescriptionText = document.getElementById("ar-description-text");
+const pcAlertText = document.getElementById("pc-alert-text");
+const noArMsg = document.getElementById("no-ar-msg");
+
 window.addEventListener("resize", () => {
   vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
@@ -19,11 +25,10 @@ if (
   ua.indexOf("android") > 0
 ) {
 } else {
-  document.getElementById("ar-description-text").style.display = "none";
-  document.getElementById("pc-alert-text").style.display = "block";
+  arDescriptionText.style.display = "none";
+  pcAlertText.style.display = "block";
 }
 
-if (ua.indexOf("iphone") > 0 || ua.indexOf("ipad") > 0) {
-  document.getElementById("model-viewer-ios").style.display = "block";
-  document.getElementById("model-viewer-default").style.display = "none";
-}
+viewer.addEventListener("load", () => {
+  document.getElementById("no-ar-msg").style.display = "block";
+});
